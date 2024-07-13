7ae5c90ae3c3a5234760394a62245ba0499fe73e
Shantanu chaudhary <shantanu.com01@gmail.com>
2024-07-13 20:16:27 +0530

    added solution

---
+++ OOP1/ComplexNumbers.java
@@ -0,0 +1,33 @@
+package OOP1;
+
+public class ComplexNumbers {
+	// Complete this class
+    private int real;
+    private int imag;
+    
+    public ComplexNumbers(int real,int imag)
+    {
+        this.real=real;
+        this.imag=imag;
+    }
+    
+    public void plus(ComplexNumbers c)
+    {
+        this.real+=c.real;
+        this.imag+=c.imag;
+    }
+    
+    public void multiply(ComplexNumbers c)
+    {
+        int newReal=((this.real)*(c.real))-(this.imag*c.imag);
+        int newImag=(this.real*c.imag)+(this.imag*c.real);
+        this.real=newReal;
+        this.imag=newImag;
+    }
+    
+    public void print()
+    {
+        System.out.print(this.real+" + i"+this.imag);
+    }
+	
+}
--- OOP1/Rough1.java
+++
@@ -1,8 +0,0 @@
-package OOP1;
-
-public class Rough1 {
-public static void main(String[] args) {
-    System.out.println("shantanu chaudhary");
-}
-}
-
